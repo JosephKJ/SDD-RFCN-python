0c2ffe0d06b56a1f861a094eee76236669380566
Joseph KJ <josephkj20@gmail.com>
2017-06-18 19:46:19 +0530

    Update

--- lib/datasets/imdb.py
+++ lib/datasets/imdb.py
@@ -103,13 +103,13 @@ class imdb(object):
         num_images = self.num_images
         widths = self._get_widths()
         for i in xrange(num_images):
-            if boxes[i][2] < boxes[i][0]:
-                boxes[i][0] = 0
             boxes = self.roidb[i]['boxes'].copy()
             oldx1 = boxes[:, 0].copy()
             oldx2 = boxes[:, 2].copy()
             boxes[:, 0] = widths[i] - oldx2 - 1
             boxes[:, 2] = widths[i] - oldx1 - 1
+            if boxes[i][2] < boxes[i][0]:
+                boxes[i][0] = 0
             assert (boxes[:, 2] >= boxes[:, 0]).all()
             entry = {'boxes' : boxes,
                      'gt_overlaps' : self.roidb[i]['gt_overlaps'],
