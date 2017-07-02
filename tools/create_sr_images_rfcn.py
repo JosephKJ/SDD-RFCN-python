7a79d3a1a0ce32e1aed6c81aa32f272ac0fa429d
Joseph KJ <josephkj20@gmail.com>
2017-07-02 07:53:02 +0530

    Update

--- tools/create_sr_images_rfcn.py
+++ tools/create_sr_images_rfcn.py
@@ -81,6 +81,7 @@ def save_detections(im, class_name, dets, path, thresh=0.5):
     for i in inds:
         bbox = dets[i, :4]
         score = dets[i, -1]
+        print bbox
         patch = im[bbox[1]:bbox[3], bbox[0]:bbox[2]]
         fname = path + '_' + class_name + '_' + str(i) + '.png'
         cv2.imwrite(fname, patch)
