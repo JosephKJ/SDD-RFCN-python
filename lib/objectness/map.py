b091a85bbd95c049597a531747ff53258106943b
Joseph KJ <josephkj20@gmail.com>
2017-08-15 15:37:47 +0530

    Update

--- lib/objectness/map.py
+++ lib/objectness/map.py
@@ -1,11 +1,22 @@
 import cv2
+import matplotlib.pyplot as plt
 
 
 def get_map(image):
     print 'Inside get_map.'
 
+
+def display_image(image):
+    plt.axis('off')
+    plt.imshow(image)
+    plt.draw()
+
+
 if __name__ == '__main__':
     print('Inside Main.')
-    # image_path = ''
-    # img = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    # get_map(img)
+    image_path = '/home/cs17mtech01001/workspace/SDD-RFCN-python/caffe/examples/images/cat.jpg'
+    img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+
+    display_image(img)
+    get_map(img)
+
