1b55a52ba369d76d59f797e213617c1379e40b26
Joseph KJ <josephkj20@gmail.com>
2017-08-15 16:02:04 +0530

    Update

--- lib/objectness/map.py
+++ lib/objectness/map.py
@@ -1,10 +1,12 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
+import os
 
 import _init_paths
 import caffe
+
+caffe_root = os.path.join(os.path.dirname(__file__), '..', '..', 'caffe')
 
 def get_map(image):
     print 'Inside get_map.'
@@ -13,13 +15,12 @@ def get_map(image):
 def display_image(image):
     plt.axis('off')
     plt.imshow(image)
-    # plt.draw()
     plt.show()
 
 
 if __name__ == '__main__':
     print('Inside Main.')
-    image_path = '/home/cs17mtech01001/workspace/SDD-RFCN-python/caffe/examples/images/cat.jpg'
+    image_path = os.path.join(caffe_root, '/examples/images/cat.jpg')
     img = cv2.imread(image_path, cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
