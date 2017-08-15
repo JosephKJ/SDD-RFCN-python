4bf869f47aa7dc0d0beeb8b87e5522e42bffa2fc
Joseph KJ <josephkj20@gmail.com>
2017-08-15 15:28:21 +0530

    Update

--- lib/objectness/map.py
+++ lib/objectness/map.py
@@ -5,7 +5,7 @@ def get_map(image):
     print 'Inside get_map.'
 
 if __name__ == 'main':
-    print 'Inside Main.'
-    image_path = ''
-    img = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    get_map(img)
+    print('Inside Main.')
+    # image_path = ''
+    # img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    # get_map(img)
