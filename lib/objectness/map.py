e288b5161ca09858ae44521aef2a348c0bac45aa
Joseph KJ <josephkj20@gmail.com>
2017-08-15 15:41:54 +0530

    Update

--- lib/objectness/map.py
+++ lib/objectness/map.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
     print('Inside Main.')
     image_path = '/home/cs17mtech01001/workspace/SDD-RFCN-python/caffe/examples/images/cat.jpg'
     img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     display_image(img)
     get_map(img)
