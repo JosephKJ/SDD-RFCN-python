02629a5dfcbd245eb4e2d6499a6c45796527e171
Joseph KJ <josephkj20@gmail.com>
2017-10-20 08:27:02 +0530

    Update

--- lib/fast_rcnn/test.py
+++ lib/fast_rcnn/test.py
@@ -227,7 +227,7 @@ def apply_nms(all_boxes, thresh):
             nms_boxes[cls_ind][im_ind] = dets[keep, :].copy()
     return nms_boxes
 
-def test_net(net, imdb, max_per_image=400, thresh=-np.inf, vis=False, refine=True):
+def test_net(net, imdb, max_per_image=400, thresh=-np.inf, vis=False, refine=False):
     """Test a Fast R-CNN network on an image database."""
     num_images = len(imdb.image_index)
     # all detections are collected into:
