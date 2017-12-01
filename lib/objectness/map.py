4507eb8be009b8f249318d94072555fcc3e6841e
Joseph KJ <josephkj20@gmail.com>
2017-12-02 05:03:34 +0530

    Update

--- lib/objectness/map.py
+++ lib/objectness/map.py
@@ -73,6 +73,9 @@ class HeatMap:
         if verbose:
             print feature_sum
 
+        # Visualizing the activations
+        self.vis_square(feat)
+
         return feature_sum
 
     def display_image(self, image):
@@ -82,6 +85,27 @@ class HeatMap:
         frame1.axes.get_yaxis().set_ticks([])
         plt.imshow(image)
         plt.show()
+
+    def vis_square(self, data):
+        """Take an array of shape (n, height, width) or (n, height, width, 3)
+           and visualize each (height, width) thing in a grid of size approx. sqrt(n) by sqrt(n)"""
+
+        # normalize data for display
+        data = (data - data.min()) / (data.max() - data.min())
+
+        # force the number of filters to be square
+        n = int(np.ceil(np.sqrt(data.shape[0])))
+        padding = (((0, n ** 2 - data.shape[0]),
+                    (0, 1), (0, 1))  # add some space between filters
+                   + ((0, 0),) * (data.ndim - 3))  # don't pad the last dimension (if there is one)
+        data = np.pad(data, padding, mode='constant', constant_values=1)  # pad with ones (white)
+
+        # tile the filters into an image
+        data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
+        data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
+
+        plt.imshow(data);
+        plt.axis('off')
 
 
 if __name__ == '__main__':
