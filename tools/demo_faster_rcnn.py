7533f0c48d37bd24531a89c549679d5f162edd2c
Joseph KJ <josephkj20@gmail.com>
2017-12-16 16:26:13 +0530

    Update

--- tools/demo_faster_rcnn.py
+++ tools/demo_faster_rcnn.py
@@ -157,7 +157,7 @@ if __name__ == '__main__':
     # im_names = ['iith_06_100.jpg', 'iith_06_250.jpg', 'iith_06_500.jpg', 'iith_06_750.jpg', 'iith_06_1000.jpg',
     # 'iith_06_1250.jpg', 'iith_06_1500.jpg', 'iith_06_1750.jpg', 'iith_06_2000.jpg', 'iith_06_2500.jpg']
     # im_names = ['bookstore_video0_12345.jpg']
-    im_names = ['iith_convo_' + str(i) + '.jpg' for i in range(1, 884)]
+    im_names = ['iith_convo_' + str(i) + '.jpg' for i in range(100, 884)]
 
     print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
     for im_name in im_names:
